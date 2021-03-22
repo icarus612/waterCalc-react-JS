92f3c19842a9c28d6cd4f312e97a244f978ddfa8
Ellis Hogan <ellishogan@Elliss-Mac-mini.local>
2021-03-22 01:25:35 -0400

    build-update

---
+++ build/precache-manifest.abd937b4adb227c7898a8943d11d34d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6511d8663f44e5bd0e605debdd7f81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e300b3727acf498cb9",
+    "url": "/static/css/main.8fa510ef.chunk.css"
+  },
+  {
+    "revision": "e7107a5407191408119a",
+    "url": "/static/js/2.8ef4f53e.chunk.js"
+  },
+  {
+    "revision": "c5e300b3727acf498cb9",
+    "url": "/static/js/main.e963beac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e262101d8ebaabda8f97ade9bdd6e9e.js"
+  "/precache-manifest.abd937b4adb227c7898a8943d11d34d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
